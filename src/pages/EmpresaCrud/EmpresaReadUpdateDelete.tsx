9bf06b6f107b7bbfda48cb310ea809bdc7f4e5cf
Pedro Henrique Cazelatto <68443795+PedroCazelatto@users.noreply.github.com>
2023-04-19 13:55:40 -0300

    Atualizar o EmpresaReadUpdateDelete.tsx

--- src/pages/EmpresaCrud/EmpresaReadUpdateDelete.tsx
+++ src/pages/EmpresaCrud/EmpresaReadUpdateDelete.tsx
@@ -64,7 +64,7 @@ export default function EmpresaReadUpdateDelete({ setAuthorized }) {
       }
     }
     )()
-  }, '');
+  }, []);
 
   const update_empresa = async () => {
     let cnpjString = String(cnpj).replace(/\D/g, '')
